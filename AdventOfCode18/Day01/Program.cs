27360a2d2875f9c98b4100cf86b3503894968b8b
Dmitriy Sheremetyev <shdv@hotmail.com>
2018-12-02 00:43:49 -0500

    1st star

---
+++ AdventOfCode18/Day01/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Day01
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int result = 0;
+            String[] freqs = System.IO.File.ReadAllLines("input.txt");
+            foreach (var freq in freqs)
+            {
+                result += int.Parse(freq);
+            }
+            Console.WriteLine(result);
+        }
+    }
+}
