6e6f73dbb2fbd1fdceb623f671b5ac4fed0fb1a9
Dmitriy Sheremetyev <shdv@hotmail.com>
2018-12-05 01:25:52 -0500

    Solved both problems for day5.

---
+++ AdventOfCode18/Day05/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Day05
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] polymer = System.IO.File.ReadAllBytes("input.txt");
+            LinkedList<byte> units = new LinkedList<byte>(polymer);
+            units.RemoveLast(); //strip off line feed character
+            bool done;
+            LinkedListNode<byte> node;
+            do
+            {
+                done = true;
+                node = units.First;
+                while (node.Next != null)
+                {
+                    if (System.Math.Abs(node.Value - node.Next.Value) == 32)
+                    {
+                        done=false;
+                        node = node.Next.Next;
+                        units.Remove(node.Previous);
+                        units.Remove(node.Previous);
+                        if (node == null) break;
+                    }
+                    else
+                    {
+                        node = node.Next;
+                    }
+                }
+            } while (!done);
+            Console.WriteLine("First answer: " + units.Count);
+            int minLength = int.MaxValue;
+            polymer = units.ToArray();
+            for (int i = 65; i < 91; i++)
+            {
+                units = new LinkedList<byte>(polymer);
+                node = units.First;
+                while(node != null)
+                {
+                    if(node.Value == i || node.Value == i+32)
+                    {
+                        node = node.Next;
+                        if(node==null)
+                        {
+                            units.RemoveLast();
+                        }
+                        else
+                        {
+                            units.Remove(node.Previous);
+                        }
+                    
+                        
+                    }
+                    else
+                    {
+                        node = node.Next;
+                    }
+                }
+                do
+                {
+                    done = true;
+                    node = units.First;
+                    while (node.Next != null)
+                    {
+                        if (System.Math.Abs(node.Value - node.Next.Value) == 32)
+                        {
+                            done = false;
+                            node = node.Next.Next;
+                            units.Remove(node.Previous);
+                            units.Remove(node.Previous);
+                            if (node == null) break;
+                        }
+                        else
+                        {
+                            node = node.Next;
+                        }
+                    }
+                } while (!done);
+                if (units.Count < minLength) minLength = units.Count;
+            }
+            Console.WriteLine("Second answer: " + minLength);
+            Console.Read();
+        }
+    }
+}
