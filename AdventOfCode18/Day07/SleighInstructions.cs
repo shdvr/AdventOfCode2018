df394b2416160d16d2722a628afc2caa5348ca7e
Dmitriy Sheremetyev <shdv@hotmail.com>
2019-01-28 19:50:31 -0500

    Completed part 1 for Day 7 using a directed graph

---
+++ AdventOfCode18/Day07/SleighInstructions.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+
+namespace Day07
+{
+    class SleighInstructions
+    {
+        static void Main(string[] args)
+        {
+            Regex instructionRegex = new Regex(@"Step ([A-Z]) must be finished before step ([A-Z]) can begin\.");
+            //read the input
+            MatchCollection instructions = instructionRegex.Matches(System.IO.File.ReadAllText("input.txt"));
+            SortedDirectedGraph g = new SortedDirectedGraph();
+            //parse it as directed edges of a graph with sorted nodes
+            foreach (Match insturction in instructions)
+            {
+                g.AddEdge((int)(insturction.Groups[1].Value[0]), (int)(insturction.Groups[2].Value[0]));
+            }
+            List<char> order = new List<char>(g.Nodes.Count);
+            //traverse the sorted graph looking for the first node with no incoming edges
+            while (g.Nodes.Count > 0)
+            {
+                foreach (var n in g.Nodes.Values)
+                {
+                    if(n.IncomingEdges.Count == 0)
+                    {
+                        //once such node is found, add it to the output list, remove it from the graph and find another one like it
+                        order.Add((char)n.ID);
+                        g.RemoveNode(n);
+                        break;
+                    }
+                }
+                //repeat until no nodes are left in the graph
+            }
+            Console.WriteLine("First answer: " + new string(order.ToArray()));
+
+            //TODO: part 2
+            //Console.WriteLine("Second answer: " + 0);
+
+            Console.Read();
+        }
+    }
+}
---
+++ AdventOfCode18/Day07/SortedDirectedGraph.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace Day07
+{
+    class SortedDirectedGraph
+    {
+        private SortedDictionary<int, Node> nodes;
+
+        public SortedDictionary<int, Node> Nodes { get => nodes; }
+
+        public SortedDirectedGraph()
+        {
+            nodes = new SortedDictionary<int, Node>();
+        }
+        public Node AddNode(int id)
+        {
+            Node newNode = new Node(id);
+            Nodes.Add(id, newNode);
+            return newNode;
+        }
+        public void AddEdge(int fromId, int toId)
+        {
+            Node from, to;
+            from = Nodes.ContainsKey(fromId) ? Nodes[fromId] : AddNode(fromId);
+            to = Nodes.ContainsKey(toId) ? Nodes[toId] : AddNode(toId);
+            AddEdge(from, to);
+        }
+        public void AddEdge(Node from, Node to)
+        {
+            Debug.Print(from.ID + " " + to.ID + Environment.NewLine);
+            from.OutgoingEdges.Add(to.ID, to);
+            to.IncomingEdges.Add(from.ID, from);
+        }
+        public void RemoveEdge(int fromId, int toId)
+        {
+            RemoveEdge(nodes[fromId], nodes[toId]);
+        }
+        public void RemoveEdge(Node from, Node to)
+        {
+            from.OutgoingEdges.Remove(to.ID);
+            to.IncomingEdges.Remove(from.ID);
+        }
+        public void RemoveNode(int id)
+        {
+            foreach (var e in nodes[id].IncomingEdges)
+            {
+                e.Value.OutgoingEdges.Remove(id);
+            }
+            foreach (var e in nodes[id].OutgoingEdges)
+            {
+                e.Value.IncomingEdges.Remove(id);
+            }
+            nodes.Remove(id);
+        }
+        public void RemoveNode(Node n)
+        {
+            foreach (var e in n.IncomingEdges)
+            {
+                e.Value.OutgoingEdges.Remove(n.ID);
+            }
+            foreach (var e in n.OutgoingEdges)
+            {
+                e.Value.IncomingEdges.Remove(n.ID);
+            }
+            nodes.Remove(n.ID);
+        }
+
+    }
+
+    class Node
+    {
+        private int nodeId;
+        public int ID { get => nodeId; }
+        SortedDictionary<int, Node> outgoingEdges;
+
+        public SortedDictionary<int, Node> OutgoingEdges { get => outgoingEdges; }
+        SortedDictionary<int, Node> incomingEdges;
+
+        public SortedDictionary<int, Node> IncomingEdges { get => incomingEdges; }
+
+        public Node(int id)
+        {
+            nodeId = id;
+            outgoingEdges = new SortedDictionary<int, Node>();
+            incomingEdges = new SortedDictionary<int, Node>();
+        }
+    }
+}
