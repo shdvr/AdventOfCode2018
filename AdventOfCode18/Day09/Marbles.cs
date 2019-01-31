25e5a039c01a163030dd1e82a08e14483629fbf7
Dmitriy Sheremetyev <shdv@hotmail.com>
2019-01-30 23:10:48 -0500

    Refactored Day 9 solution to calculate answers to both parts in one run

--- AdventOfCode18/Day09/Marbles.cs
+++ AdventOfCode18/Day09/Marbles.cs
@@ -13,14 +13,21 @@ namespace Day09
             MatchCollection inputData = inputRegex.Matches(System.IO.File.ReadAllText("input.txt"));
             int playerCount = int.Parse(inputData[0].Groups[1].Value);
             int marbleCount = int.Parse(inputData[0].Groups[2].Value);
-            int[] scores = new int[playerCount]; //set up the scorecard
+            Console.WriteLine("First answer: " + PlayGame(playerCount, marbleCount));
+            Console.WriteLine("Second answer: " + PlayGame(playerCount, 100 * marbleCount));
+            Console.Read();
+        }
+
+        private static long PlayGame(int playerCount, int highestMarble)
+        {
+            long[] scores = new long[playerCount]; //set up the scorecard
             LinkedList<int> circle = new LinkedList<int>(new int[] { 0 }); //put the first marble down
             LinkedListNode<int> current = circle.First;
             int nextMarblePoints = 1; //value of the next marble to play
             int currentPlayer = 0; //player that will be playing the next marble
-            while (nextMarblePoints <= marbleCount) //play until we run out of marbles
+            while (nextMarblePoints <= highestMarble) //play until we run out of marbles
             {
-                if(nextMarblePoints % 23 == 0)
+                if (nextMarblePoints % 23 == 0)
                 {
                     scores[currentPlayer] += nextMarblePoints;
                     for (int i = 0; i < 6; i++)
@@ -28,7 +35,7 @@ namespace Day09
                         current = current.Previous;
                         if (current == null) current = circle.Last;
                     }
-                    if(current.Previous == null)
+                    if (current.Previous == null)
                     {
                         scores[currentPlayer] += circle.First.Value;
                         circle.RemoveFirst();
@@ -50,11 +57,7 @@ namespace Day09
                 currentPlayer++;
                 if (currentPlayer == playerCount) currentPlayer = 0;
             }
-   
-            Console.WriteLine("First answer: " + scores.Max());
-
-            //Console.WriteLine("Second answer: " + checksum);
-            Console.Read();
+            return scores.Max();
         }
     }
 }
